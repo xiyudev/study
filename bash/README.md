16e097b53073e569f6afa5c28f4c962b3b8038b0
Xiyu Wang <sionwxy@gmail.com>
2024-07-15 00:06:12 -0700

    [bash] Add a basic bash alias script.

---
+++ bash/README.md
@@ -0,0 +1,6 @@
+# Setting up and Learning Bash
+
+This folder contains:
+- My common bash aliases (like git).
+- Script for installing common bash tools.
+- Bash scripts.
---
+++ bash/rc/aliases.sh
@@ -0,0 +1,21 @@
+# =============================================================================
+# Common Bash aliases
+# =============================================================================
+
+# -----------------------------------------------------------------------------
+# git
+# -----------------------------------------------------------------------------
+# Copied from https://github.com/SionWxy/dots/blob/main/env/alias.bash.
+# That version is more actively maintained.
+alias gs='git status'
+alias gf='git fetch'
+alias gd='git diff'
+alias gdc='git diff --cached'
+alias gl='git log --oneline --graph --decorate -n10'
+alias gll='git log --oneline --graph --decorate -n20'
+alias glll='git log --oneline --graph --decorate -n30'
+alias gllll='git log --oneline --graph --decorate -n50'
+# alias gll='git log --oneline --graph --decorate --stat -n5'
+alias gla='git log --oneline --graph --decorate --all -n10'
+alias gco='git checkout'
+alias gcb='git checkout -b'
